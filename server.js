4a265398a1793b53f30ba03f060de11fd7025582
kqarlos <kqarlos@hotmail.com>
2020-02-04 23:20:50 -0800

    Organized api routes

--- server.js
+++ server.js
@@ -6,8 +6,6 @@ const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 
-const db = require("./models");
-
 const app = express();
 
 app.use(logger("dev"));
@@ -21,6 +19,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { use
 
 // require("./seeders/seed.js");
 
+require("./routes/api-routes.js")(app);
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname + "/public/index.html"));
@@ -33,81 +32,6 @@ app.get("/exercise", (req, res) => {
 app.get("/stats", (req, res) => {
     res.sendFile(path.join(__dirname + "/public/stats.html"));
 });
-
-//get workouts
-app.get("/api/workouts", (req, res) => {
-
-    db.Workout.find({}).then(dbWorkout => {
-        console.log("ALL WORKOUTS");
-        console.log(dbWorkout);
-        dbWorkout.forEach(workout => {
-            var total = 0;
-            workout.exercises.forEach(e => {
-                console.log("Exercise");
-                console.log(e);
-                total += e.duration;
-            });
-            workout.totalDuration = total;
-
-        });
-
-        res.json(dbWorkout);
-    }).catch(err => {
-        res.json(err);
-    });
-});
-
-// add exercise
-app.put("/api/workouts/:id", (req, res) => {
-    console.log("WORKOUT ID TO ADD EXERCISE");
-    console.log(req.params.id);
-    console.log("EXCERCISE TO BE ADDED");
-    console.log(req.body);
-
-    db.Workout.findOneAndUpdate(
-        { _id: req.params.id },
-        {
-            $inc: { totalDuration: req.body.duration },
-            $push: { exercises: req.body }
-        },
-        { new: true }).then(dbWorkout => {
-            res.json(dbWorkout);
-        }).catch(err => {
-            res.json(err);
-        });
-
-
-
-
-});
-
-//create workout
-app.post("/api/workouts", ({ body }, res) => {
-    console.log("WORKOUT TO BE ADDED");
-    console.log(body);
-
-    db.Workout.create(body).then((dbWorkout => {
-        res.json(dbWorkout);
-    })).catch(err => {
-        res.json(err);
-    });
-});
-
-// get workouts in range
-app.get("/api/workouts/range", (req, res) => {
-
-    db.Workout.find({}).then(dbWorkout => {
-        console.log("ALL WORKOUTS");
-        console.log(dbWorkout);
-
-        res.json(dbWorkout);
-    }).catch(err => {
-        res.json(err);
-    });
-
-});
-
-
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
